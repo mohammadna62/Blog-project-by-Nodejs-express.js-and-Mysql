5c0529cc97bf755d7c07073a26b0c342f7411558
mohammad naghavi <mohammad.naghavi@gmail.com>
2026-04-19 22:56:22 +0330

    develope register method on controller.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,51 @@
-exports.register = async (req, res,next) => {};
+const User = require("./../repositories/users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const configs = require("./../configs");
 
-exports.login = async (req, res,next) => {};
+exports.register = async (req, res, next) => {
+  try {
+    const { name, username, email, password } = req.body;
+    //*  Validation
 
-exports.refresh = async (req, res,next) => {};
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    });
 
-exports.getMe = async (req, res,next) => {};
+    const accessToken = jwt.sign(
+      { id: user.id, role: user.role },
+      configs.auth.accessTokenSecretKey,
+      {
+        expiresIn: configs.auth.accessTokenExpiresInSeconds + "s",
+      },
+    );
+    const refreshToken = jwt.sign(
+      { id: user.id, role: user.role },
+      configs.auth.refreshTokenSecretKey,
+      {
+        expiresIn: configs.auth.refreshTokenExpiresInSeconds + "s",
+      },
+    );
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 12);
+    //! use below codes for api base project
+    return res.status(201).json({
+      accessToken,
+      refreshToken: hashedRefreshToken,
+    });
+  } catch (err) {
+    
+    next(err);
+  }
+};
 
-exports.logout = async (req, res,next) => {};
+exports.login = async (req, res, next) => {};
+
+exports.refresh = async (req, res, next) => {};
+
+exports.getMe = async (req, res, next) => {};
+
+exports.logout = async (req, res, next) => {};
--- repositories/users.js
+++ repositories/users.js
@@ -1,27 +1,30 @@
 const db = require("./../db");
 const create = async ({ name, username, email, password }) => {
+  /* const insertQuery =
+    "insert into users (name, username,email,password) values (?,?,?,?) returning *"; */ //! (returning *) this code on the insert query used for mariadb on mysql 10.5 version . this code help us to have not to use a repeated a select query after insert like below  "select * from users where id=? "
   const insertQuery =
     "insert into users (name, username,email,password) values (?,?,?,?)";
 
-  const [user] = await db.execute(insertQuery, [
+  const [insertedUser] = await db.execute(insertQuery, [
     name,
     username,
     email,
     password,
   ]);
+  const selectMainUser = "select * from users where id=? ";
+  const user = await db.execute(selectMainUser, [insertedUser.insertedId]);
+  return user[0][0];
+};
+const findByUsername = async (username) => {
+  const query = "select * from users where username =?";
+  const [user] = await db.execute(query, [username]);
   return user[0];
 };
-const findByUsername = async (username)=>{
-  const query = "select * from users where username =?"
-  const [user] = await db.execute(query,[username])
-  return user[0]
-}
 
-const findById = async (id)=>{
-    const query ="select * from users where id = ?"
-    const [user] = await db.query(query , [id])
-    return user[0]
-}
+const findById = async (id) => {
+  const query = "select * from users where id = ?";
+  const [user] = await db.query(query, [id]);
+  return user[0];
+};
 
-
-module.exports = { create , findByUsername , findById};
+module.exports = { create, findByUsername, findById };
