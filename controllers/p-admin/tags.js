85ada1a572cf3925605891e3baf808a74fb59d56
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-04-28 12:47:22 +0330

    CKEditor config

--- controllers/p-admin/tags.js
+++ controllers/p-admin/tags.js
@@ -3,9 +3,17 @@ exports.showTagsManagement = async(req , res , next)=>{
     try {
         const tags = await Tag.findAll()
 
-        return res.render('p-admin/tags',{tags})
+        return res.render('p-admin/tags.ejs',{tags})
     } catch (err) {
         next(err)
         
+    }
+}
+
+exports.showCreateArticle= async (req , res , next)=>{
+    try {
+        return res.render('p-admin/createArticle.ejs')
+    } catch (err) {
+        next(err)
     }
 }
--- routes/p-admin.js
+++ routes/p-admin.js
@@ -5,5 +5,6 @@ const authGuard = require("./../middlewares/authGuard")
 const router = express.Router();
 
 router.route("/tags").get(authGuard,controller.showTagsManagement);
+router.route('/create-article').get(authGuard,controller.showCreateArticle)
 
 module.exports = router;
