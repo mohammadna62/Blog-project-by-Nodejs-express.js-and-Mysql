26129d43315c2321c1953cd933ce0f75023ab588
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-04-29 12:07:48 +0330

    config sharp package for compress cover picture

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,7 @@
+const sharp = require("sharp");
 const Article = require("./../repositories/articles");
 const slugify = require("slugify");
+const path = require("path");
 exports.getAll = async (req, res, next) => {
   try {
   } catch (err) {
@@ -9,16 +11,41 @@ exports.getAll = async (req, res, next) => {
 
 exports.create = async (req, res, next) => {
   try {
-    let { title, content, slug , tags} = req.body;
+    let { title, content, slug, tags } = req.body;
     slug = slugify(slug, {
       lower: true,
     });
     const author_id = req.user.id;
-      
-   const article = await Article.create({title, content, slug, author_id, cover:req.file?.filename});
-    tags.forEach(async(tag)=>{
-       await Article.addTag(article.id,Number(tag))
-    })
+    const fileBuffer = req.file.buffer;
+    const coverPath = `./public/images/covers/${Date.now()}-${req.file.originalname}`;
+    const extType = path.extname(req.file.originalname);
+
+    if (extType === ".png") {
+      sharp(fileBuffer)
+        .png({
+          quality: 60,
+        })
+        .toFile(coverPath);
+    } else if (extType === ".jpeg" || extType === ".jpg") {
+      sharp(fileBuffer)
+        .jpeg({
+          quality: 60,
+        })
+        .toFile(coverPath);
+    } else {
+      next(new Error("File Type Is Not Valid !"));
+    }
+
+    // const article = await Article.create({
+    //   title,
+    //   content,
+    //   slug,
+    //   author_id,
+    //   cover: req.file?.filename,
+    // });
+    // tags.forEach(async (tag) => {
+    //   await Article.addTag(article.id, Number(tag));
+    // });
     req.flash("success", "مقاله مورد نظر ایجاد شد");
     return res.redirect("/p-admins/create-article");
   } catch (err) {
--- routes/articles.js
+++ routes/articles.js
@@ -2,34 +2,37 @@ const express = require("express");
 const controller = require("./../controllers/articles");
 const multer = require("multer");
 const path = require("path")
-const authGuard = require("./../middlewares/authGuard")
+const authGuard = require("./../middlewares/authGuard");
+const { date } = require("yup");
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null , path.resolve(__dirname,"../public/images/covers"))
-  },
-  filename: (req, file, cb) => {
-    const filename = `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`
-    cb(null ,filename )
-  },
-});
+//! When use sharp package to compress file(picture cover) no need to config storage
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null , path.resolve(__dirname,"../public/images/covers"))
+//   },
+//   filename: (req, file, cb) => {
+     
+//     const filename = `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`
+//     cb(null ,filename )
+//   },
+// });
 const fileFilter = (req , file ,cb) =>{
    const validFileTypes = /jpeg|jpg|png/
-   const mimType = validFileTypes.test(file.mimetype)
+   const mimeType = validFileTypes.test(file.mimetype)
    const extName = validFileTypes.test(path.extname(file.originalname))
-   if (mimType && extName){
+   if (mimeType && extName){
    return cb(null ,true )
    }else{
     return  cb(new Error("File Type Is Not Valid !"))
    }
 }
 const uploader = multer({
-    storage, //! storage : storage
+   // storage, //! storage : storage  Note: When use sharp package to compress file(picture cover) no need to config storage
     fileFilter,//! fileFilter:fileFilter
     limits:{fileSize:3*1024*1024} //#MB
 })
+
 
 router.route("/").get(controller.getAll).post(authGuard,uploader.single("cover"),controller.create);
 router.route("/:slug").get(controller.getBySlug);
